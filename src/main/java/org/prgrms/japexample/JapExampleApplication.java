7e30d4febff85e89dceeeb6f2cd3a9e4127246b8
박세준 <tpwns6693@gmail.com>
2022-12-10 02:24:43 +0900

    jdbc 실습

---
+++ src/main/java/org/prgrms/japexample/JapExampleApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.japexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JapExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JapExampleApplication.class, args);
+    }
+
+}
