571ad7ac26b8632ef855ba69768ad3d14a7c04c0
박세준 <tpwns6693@gmail.com>
2022-12-11 18:46:06 +0900

    jpa 테스트 실습

---
+++ src/main/java/org/prgrms/japexample/lecture/repository/CustomerRepository.java
@@ -0,0 +1,7 @@
+package org.prgrms.japexample.lecture.repository;
+
+import org.prgrms.japexample.lecture.repository.domain.CustomerEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+//jpaRepository를 상속받아서 해당 엔티티의 레포지토리를 만든다.
+public interface CustomerRepository extends JpaRepository<CustomerEntity,Long> {
+}
---
+++ src/test/java/org/prgrms/japexample/JPATest.java
@@ -0,0 +1,64 @@
+package org.prgrms.japexample;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.prgrms.japexample.lecture.repository.CustomerRepository;
+import org.prgrms.japexample.lecture.repository.domain.CustomerEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@Slf4j
+@SpringBootTest
+public class JPATest {
+
+    @Autowired
+    CustomerRepository repository;
+
+    @BeforeEach
+    void setUp(){}
+    @AfterEach
+    void tearDown(){
+        repository.deleteAll();
+    }
+
+    @Test
+    void INSERT_TEST(){
+        CustomerEntity customer = new CustomerEntity();
+        customer.setId(1L);
+        customer.setFirstName("honggu");
+        customer.setLastName("kang");
+        customer.setAge(23);
+
+        //when
+        repository.save(customer);
+
+        //then
+        var entity = repository.findById(1L).get();
+        log.info("{} {}",entity.getFirstName(),entity.getLastName());
+        log.info("{}",customer.getAge());
+    }
+
+    @Test
+    @Transactional
+    void UPDTE_TEST(){
+        //given
+        CustomerEntity customer = new CustomerEntity();
+        customer.setId(1L);
+        customer.setFirstName("honggu");
+        customer.setLastName("kang");
+        repository.save(customer);
+        //when
+        var entity = repository.findById(1L).get();
+        entity.setFirstName("quppy");
+        entity.setLastName("hong");
+
+        //then
+        var updated = repository.findById(1L).get();
+
+        log.info("{} {}",updated.getFirstName(),updated.getLastName());
+
+    }
+}
